0ce23627c243e220b53c84f1a0c76c7d0fd6996c
damithc <damith@gmail.com>
2020-02-04 17:20:58 +0800

    Trainer.java: tweak casting steps

--- src/main/java/Trainer.java
+++ src/main/java/Trainer.java
@@ -3,11 +3,12 @@ public class Trainer {
         Duck d = new Duck();
         System.out.println(d.speak());
         Bird b = (Bird)d;  // upcasting
-        System.out.println(b.speak());
-        Duck d2 = (Duck) b; // downcasting
+        Animal a = (Animal)b; // upcasting
+        System.out.println(a.speak());
+        Duck d2 = (Duck) a; // downcasting
         train(new Duck());
         // train(new Parrot());
-        Animal a = new Animal();
+        Animal a2 = new Animal();
         Bird b2 = new Bird();
     }
 
