803ab50746752673dc2696c335b5b922d32dd6f1
Akshay <okkhoy@gmail.com>
2020-09-04 13:01:13 +0800

    Use polymorphism in Trainer

--- src/main/java/Trainer.java
+++ src/main/java/Trainer.java
@@ -1,15 +1,23 @@
 public class Trainer {
     public static void main(String[] args) {
         Duck d = new Duck();
-        System.out.println(d.speak());
+        getToSpeak(d);
+
         Bird b = (Bird)d;  // upcasting
+        getToSpeak(b);
+
         Animal a = (Animal)b; // upcasting
-        System.out.println(a.speak());
+        getToSpeak(a);
+
         Duck d2 = (Duck) a; // downcasting
         train(new Duck());
         // train(new Parrot());
         Animal a2 = new Animal();
         Bird b2 = new Bird();
+    }
+
+    private static void getToSpeak(Animal animal) {
+        System.out.println(animal.speak());
     }
 
     private static void train(Bird bird) {
