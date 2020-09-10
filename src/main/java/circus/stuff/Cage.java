7134e94ef37904a418d6f0f28681628f2cb0047a
Akshay <okkhoy@gmail.com>
2020-09-10 21:51:44 +0800

    Add cage class

---
+++ src/main/java/circus/stuff/Cage.java
@@ -0,0 +1,17 @@
+package circus.stuff;
+
+import circus.animal.Animal;
+
+public class Cage<T> {
+    private T t;
+
+    public void lockUp(T t) {
+        this.t = t;
+        System.out.println("In the cage: " + t);
+    }
+
+    public T release() {
+        System.out.println(t + " out of cage");
+        return t;
+    }
+}
