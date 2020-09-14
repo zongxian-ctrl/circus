36a0a22c213993fde9cbdfc5e30ab925f76ed1ac
Zong Xian <zongxian96@gmail.com>
2020-09-15 01:00:56 +0800

    no message

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -3,6 +3,7 @@ package circus;
 import circus.animal.Animal;
 import circus.animal.Duck;
 import circus.animal.Parrot;
+import circus.animal.Tiger;
 import circus.stuff.Cage;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
@@ -79,16 +80,23 @@ public class Circus {
         makeAnimalsTalk();
         System.out.println("Total value of equipments " + calculateValue(equipments));
         System.out.println("Total value of animals " + calculateValue(animals));
+
         Cage<Duck> duckCage = new Cage<>();
         Duck duck = new Duck("Heuye");
         duckCage.lockUp(duck);
+
         Parrot parrot = new Parrot("Popper");
         Cage<Parrot> parrotCage = new Cage<>();
         parrotCage.lockUp(parrot);
 
+        Tiger tiger = new Tiger("Sherkhan");
+        Cage <Tiger> tigerCage = new Cage<>();
+        tigerCage.lockUp(tiger);
+
         ArrayList<Cage> cages = new ArrayList<>();
         cages.add(duckCage);
         cages.add(parrotCage);
+        cages.add(tigerCage);
 
         for(Cage c: cages) {
             c.release();
